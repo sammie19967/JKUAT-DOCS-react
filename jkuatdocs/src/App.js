ae097e6aad1bbc0744028daf428c6892f1a2290d
sammie19967 <barasasam002@gmail.com>
2024-04-12 16:44:50 +0300

    Initial commit

---
+++ jkuatdocs/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './Navbar';
+
+function App() {
+   
+  return (
+    <div className="App">
+       <Navbar/>
+      <div className="content">
+        <h1>Welcome To JKUAT DOCS</h1>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ jkuatdocs/src/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>JKUAT DOCS</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Chairman</a>
+                <a href="/contact">Registry</a>
+                <a href="/login">Student</a>
+                <a href="/register">Hiring Company</a>
+            </div>
+        </nav>
+
+     );
+}
+ 
+export default Navbar;
